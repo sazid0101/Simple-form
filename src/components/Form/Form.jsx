50ed8c7e4adb1e05c382b74d29e7ef1c81fb3f4a
sazid0101 <shahriarislamsazid@gmail.com>
2025-04-13 16:01:38 +0600

    form add

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 
 const Form = () => {
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();  //use for don't load the page.
+        console.log(e.target.name.value)
+        console.log(e.target.email.value)
+        console.log(e.target.phone.value)
+
+    }
     return (
         <div>
-            <form>
-                <input type="text" /> <br />
-                <button>Submit</button>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name='name' placeholder='Name'/>
+                <br />
+                <input type="text" name='email' placeholder='Email' />
+                 <br />
+                 <input type="text" name='phone' placeholder='phone' />
+                 <br />
+                {/* <button>Submit</button> */}
+                <input type="submit" value="submit" />
             </form>
         </div>
     );
