8b24f69f42381edd1270e72f6441f5f008a7c8b6
sazid0101 <shahriarislamsazid@gmail.com>
2025-04-13 15:40:55 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Form from './components/Form/form'
+function App() {
+  
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <Form></Form>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Form = () => {
+    return (
+        <div>
+            <form>
+                <input type="text" /> <br />
+                <button>Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default Form;
